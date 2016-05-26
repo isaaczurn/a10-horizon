de5b0e387f31201097ac45b7bab47b7764d54ab6
Michael Durrant <mdurrant@a10networks.com>
2016-05-26 15:57:32 +0000

    Typo fix

--- a10_horizon/dashboard/a10networks/a10scaling/tables.py
+++ a10_horizon/dashboard/a10networks/a10scaling/tables.py
@@ -356,11 +356,11 @@ class A10ScalingGroupTable(tables.DataTable):
 
 
 class A10ScalingGroupMemberTable(tables.DataTable):
-    id = tables.Column("id", verbose_name=_("ID"))
+    id = tables.Column("id", verbose_name=_("ID"), hidden=True)
     scaling_group_id = tables.Column("scaling_group_id", verbose_name=_("Scaling Group ID"),
                                      hidden=True)
     tenant_id = tables.Column("tenant_id", verbose_name=_("Tenant ID"), hidden=True)
-    name = tables.Column("name", verbose_name=("Name"), hidden=True)
+    name = tables.Column("name", verbose_name=("Name"))
     host = tables.Column("host", verbose_name=_("Host"))
     nova_instance_id = tables.Column("nova_instance_id", verbose_name="Instance ID")
 
