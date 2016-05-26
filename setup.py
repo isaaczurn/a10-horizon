13f5db37b536d78cf1896c1b7195d86311f50101
Doug Wiegley <doug@parksidesoftware.com>
2016-05-26 00:57:07 -0600

    bump to 0.1.4a

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "a10-horizon",
-    version = "0.1.3",
+    version = "0.1.4a",
     packages = find_packages(),
 
     author = "A10 Networks",
