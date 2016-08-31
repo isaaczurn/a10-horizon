16fb6f25bf308e4a5252d5163934db5d0e1c3846
isaaczurn <zurn.isaac@gmail.com>
2016-08-31 16:36:39 -0600

    Removed unnecessary commented code

--- a10_horizon/dashboard/admin/a10networks/instances/helper.py
+++ a10_horizon/dashboard/admin/a10networks/instances/helper.py
@@ -33,44 +33,10 @@ def get_hosts(request, filter=None):
     return host_list
 
 def get_result(request, results):
-#    if len(results) > 0:
-#        servers = nova_api.server_list(request) #get list of servers
-#        server_ids = [x["nova_instance_id"] for x in results] #get list of nova id's from results
-def get_result(request, results):
     if len(results) > 0:
         servers = nova_api.server_list(request) #get list of servers
         server_ids = [x["nova_instance_id"] for x in results] #get list of nova id's from results
-#        LOG.info("SERVER_ID")
-#        LOG.info(servers)
-#        LOG.info(servers["servers"])
-#        instance_server_ids = []
-#        for x in servers["servers"]:
-#            if x["id"] in server_ids:
-#                instance_server_ids.append(x["id"])
-#        instance_servers = [nova_api.server_get(request, x) for x in instance_server_ids] # list of full server object dictionaries
-#        flavors = nova_api.flavor_list(request)
-#        server_flavors = [x["flavor"] for x in instance_servers] # should be a list of flavor dictionaries
-#        instance_flavors = [x for x in flavors["flavors"] if x["id"] in server_flavors]
-#        tenants= keystone_api.tenant_list(request)
-#
-        #instance_servers = [nova_api.server_get(request, x) for x in instance_server_ids] # list of full server object dictionaries
-        #flavors = nova_api.flavor_list(request)
-        #server_flavors = [x["flavor"] for x in instance_servers] # should be a list of flavor dictionaries
-        #instance_flavors = [x for x in flavors["flavors"] if x["id"] in server_flavors]
-        #tenants= keystone_api.tenant_list(request)
 
-#        for instance in results:
-#            for server in instance_servers:
-#                if instance["nova_instance_id"] == server["id"]:
-#                    for flavor in instance_flavors:
-#                        if server["flavor"]["id"] == flavor["id"]:
-#                            instance["flavor"] = flavor
-#
-#                    for tenant in tenants:
-#                        if server["tenant_id"] == tenant["id"]:
-#                            instance["owner"] = tenant["name"]
-#
-                   # instance["image"] = glance_api.image_get(request, server["image"]["id"])
     result_list = []
     for instance in results:
         server = nova_api.server_get(request, instance["nova_instance_id"])
@@ -92,8 +58,6 @@ def migrate(request, id, host):
         nova_api.server_live_migrate(request, id, host)
     except Exception:
         LOG.exception("Failure to migrate.")
-#                    instance["image"] = glance_api.image_get(request, server["image"]["id"])
-#
         return results
 
 def migrate(request, id, host):
